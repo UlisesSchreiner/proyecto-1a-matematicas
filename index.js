8d433898c15c5c7eb3224cce6fc3dce737ba4872
UlisesSchreiner <schreinerulises@gmail.com>
2023-04-16 19:31:43 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  /**
+   *
+   * @param {*} n1 de la suma
+   * @param {*} n2 de la suma
+   * @returns number
+   */
+  suma: function (n1, n2) {
+    return n1 + n2;
+  },
+  resta: function (n1, n2) {
+    return n1 - n2;
+  },
+  multiplicacion: function (n1, n2) {
+    return n1 * n2;
+  },
+  division: function (n1, n2) {
+    return n1 / n2;
+  },
+};

555bfe29f6d0f764112aac0e21908ee97084751d
UlisesSchreiner <schreinerulises@gmail.com>
2023-04-16 20:17:14 -0300

    new funcionality

--- index.js
+++ index.js
@@ -6,15 +6,24 @@ module.exports = {
    * @returns number
    */
   suma: function (n1, n2) {
-    return n1 + n2;
+    return this.esNumero(n1, n2) ? n1 + n2 : this.mensajeError();
   },
   resta: function (n1, n2) {
-    return n1 - n2;
+    return this.esNumero(n1, n2) ? n1 - n2 : this.mensajeError();
   },
   multiplicacion: function (n1, n2) {
-    return n1 * n2;
+    return this.esNumero(n1, n2) ? n1 * n2 : this.mensajeError();
   },
   division: function (n1, n2) {
-    return n1 / n2;
+    return this.esNumero(n1, n2) ? n1 / n2 : this.mensajeError();
+  },
+  mensajeError: function () {
+    console.log("Error numerico");
+  },
+  esNumero: function (n1, n2) {
+    if (typeof n1 !== "number" || typeof n2 !== "number") {
+      return false;
+    }
+    return true;
   },
 };
